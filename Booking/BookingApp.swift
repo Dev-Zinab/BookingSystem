270609c4801eecdc8369572efedea04364605531
Dev-Zinab <St.biology1991@gmail.com>
2026-03-10 02:20:48 +0300

    Initial Commit

---
+++ Booking/BookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookingApp.swift
+//  Booking
+//
+//  Created by Zinab Zooba on 10/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct BookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Booking/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Booking
+//
+//  Created by Zinab Zooba on 10/03/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
