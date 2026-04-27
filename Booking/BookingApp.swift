ff70ca390d61fa759cef7daf8b8b30c28912b6af
Dev-Zinab <St.biology1991@gmail.com>
2026-04-27 18:53:24 +0300

    model

--- Booking/BookingApp.swift
+++ Booking/BookingApp.swift
@@ -5,10 +5,27 @@
 //  Created by Zinab Zooba on 10/03/2026.
 //
 
+
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+
+        FirebaseApp.configure()
+        return true
+    }
+}
 
 @main
 struct BookingApp: App {
+
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
--- Booking/User.swift
+++ Booking/User.swift
@@ -1,10 +1,47 @@
-//
-//  Model.swift
-//  Booking
-//
-//  Created by Zinab Zooba on 27/04/2026.
-//
-ْ
-user {
+
+
+import Foundation
+
+struct User: Codable,Identifiable {
+    let id: UUID
+    var name: String
+    let email: String
+    let password: String
+}
+struct Admin: Codable,Identifiable {
+    let id: UUID
+    var name: String
+    var email: String
+    var password: String
+}
+
+struct Booking: Codable,Identifiable {
+    let id: UUID
+    var userId: UUID
+    var date: Date
+    var duration: Int
+    var roomId: UUID
+    var status: String
+}
+
+struct Room: Codable,Identifiable {
+    let id: UUID
+    var name: String
+    var capacity: Int
+    var price: Double
+}
+
+struct slot: Codable,Identifiable {
+    let id: UUID
+    var userId: UUID
+    var date: Date
+    var status: String
+    var roomId: UUID
     
+}
+
+enum status: String, Codable {
+    case booked = "booked"
+    case cancelled = "cancelled"
+    case available = "available"
 }
